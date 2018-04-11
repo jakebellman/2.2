9a0393dcd99513727445ce36b2f4e2f33dcbd6e6
jakebellman <37170005+jakebellman@users.noreply.github.com>
2018-04-11 12:45:07 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey;
+
+		}
+	}
+}
