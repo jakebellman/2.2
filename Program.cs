8c73831da997afdfd4d5a68bc139db8e0381a278
jakebellman <37170005+jakebellman@users.noreply.github.com>
2018-04-11 12:56:00 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Application
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello Worlrrrrrrrd!");
 			Console.ReadKey;
 
 		}
